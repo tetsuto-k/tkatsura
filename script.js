2be0bfde221a57cb072251e6bd2b535d3429c745
tetsuto-k <tkatsura@ucsb.edu>
2023-12-20 10:26:59 -0800

    references

---
+++ script.js
@@ -0,0 +1,11 @@
+// script.js
+
+// Function to show the popup
+function showPopup() {
+    document.getElementById("myPopup").style.display = "block";
+}
+
+// Function to hide the popup
+function hidePopup() {
+    document.getElementById("myPopup").style.display = "none";
+}
