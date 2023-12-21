1da7737d2425c42d5c8d0f7b6e796d9fc215e5cc
tetsuto-k <tkatsura@ucsb.edu>
2023-12-21 13:16:57 -0800

    overlay cover

--- script.js
+++ script.js
@@ -3,18 +3,21 @@
 function showPopup(htmlFileName) {
     // Get the popup container
     const popupContainer = document.getElementById("popupContainer");
-    var overlay = document.querySelector('.overlay');
+    var overlayElement = document.querySelector('.overlay');
 
     // Load content from the external HTML file
     fetch(htmlFileName)
         .then(response => response.text())
         .then(content => {
+            // Block everyything in the main website
+            overlayCover.style.display = 'block';
+
             // Set the content inside the popup container
             popupContainer.innerHTML = content;
 
             // Show the popup
             popupContainer.classList.add("active");
-            overlay.style.display = 'block';
+            overlayElement.style.display = 'block';
         
         });
 }
@@ -22,5 +25,6 @@ function showPopup(htmlFileName) {
 function hidePopup() {
     // Hide the popup
     document.getElementById("popupContainer").classList.remove("active");
-    overlay.style.display = 'none';
+    var overlayElement = document.querySelector('.overlay');
+    overlayElement.style.display = 'block';
 }
