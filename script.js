58952cba1000982b30f1212fb4b88a5d57f2d93d
tetsuto-k <tkatsura@ucsb.edu>
2023-12-21 16:53:29 -0800

    GG

--- script.js
+++ script.js
@@ -13,15 +13,18 @@ function showPopup(htmlFileName) {
             popupContainer.innerHTML = content;
 
             // Show the popup
+            console.log("Nothing visible yet");
             popupContainer.style.visibility = "visible";
             setTimeout(function () {
                 popupContainer.classList.add("active");
               }, 100);
-
+            console.log("just set timeout for first");
             // Block everything in the main website
             overlayBackground.style.visibility = "visible";
+            console.log("2nd is visible");
             setTimeout(function () {
                 overlayBackground.classList.add("active");
+                console.log("time out 2nd");
               }, 100);
         });
 }
@@ -33,8 +36,6 @@ function hidePopup() {
     
     setTimeout(function () {
         document.getElementById("popupContainer").style.visibility = "hidden";
-      }, 900);
-    setTimeout(function () {
         document.getElementById("overlayBackground").style.visibility = "hidden";
       }, 900);
 }
