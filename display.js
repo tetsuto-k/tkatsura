f8f11f9b3468d478a5aac0534b84a313a58f776f
tetsuto-k <tkatsura@ucsb.edu>
2024-01-03 10:30:50 -0800

    fetch

--- display.js
+++ display.js
@@ -1,6 +1,10 @@
 fetch('popup.html')
     .then(response => response.text())
     .then(content => {
-        document.getElementById('displayText').innerText = document.getElementById('moveToIndex').innerText;
+        const parser = new DOMParser();
+        const popupDoc = parser.parseFromString(content, 'text/html');
+        const moveToIndex = popupDoc.getElementById('moveToIndex').innerHTML;
+
+        document.getElementById('displayText').innerText = moveToIndex;
     })
     .catch(error => console.error('Error fetching content:', error));
