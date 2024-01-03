cab43688ee044af971364435f8c0ed76905d807c
tetsuto-k <tkatsura@ucsb.edu>
2024-01-03 10:27:22 -0800

    fix fetch

--- display.js
+++ display.js
@@ -1,6 +1,6 @@
 fetch('popup.html')
     .then(response => response.text())
     .then(content => {
-        document.getElementById('displayText').innerText = document.getElementById('moveToIndex');
+        document.getElementById('displayText').innerText = document.getElementById('moveToIndex').innerText;
     })
     .catch(error => console.error('Error fetching content:', error));
